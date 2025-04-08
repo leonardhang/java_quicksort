36075e9f2347263c02ab41c03fd7fa96568aac3b
Leonardhang <Leonardhang@XA-CRM-LEONARD>
2025-04-08 09:59:07 +0800

    remove useless memo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,8 +6,6 @@ import java.util.Objects;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
-//TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
-// 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
 public class Main {
     public static void main(String[] args) {
         System.out.print("请输入10个整数，以空格分开：");
